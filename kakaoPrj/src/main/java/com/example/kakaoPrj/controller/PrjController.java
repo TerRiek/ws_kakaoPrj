e841151d143f4de14dda6d76eda57d97a518cf09
Kim ye young <kimyyoung1018@gmail.com>
2024-05-02 11:30:01 +0900

    index.html / loginForm.html / 기본 css 파일 / 컨트롤러 기본 세팅 작업

--- kakaoPrj/src/main/java/com/example/kakaoPrj/controller/PrjController.java
+++ kakaoPrj/src/main/java/com/example/kakaoPrj/controller/PrjController.java
@@ -1,17 +1,32 @@
 package com.example.kakaoPrj.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.example.kakaoPrj.dao.IMemberDao;
+import com.example.kakaoPrj.dao.INoticeDao;
 
 import lombok.extern.log4j.Log4j2;
 
 @Controller
 @Log4j2
 public class PrjController {
-
-	@RequestMapping("/aaa")
-	public @ResponseBody String root() {
-		return "aaa";
+	
+	@Autowired
+	private IMemberDao mdao;
+	
+	@Autowired
+	private INoticeDao ndao;
+	
+	@RequestMapping("/")
+	public String root() {
+		return "index";
+	}
+	
+	@RequestMapping("/loginForm")
+	public String login() {
+		return "loginForm";
 	}
 }

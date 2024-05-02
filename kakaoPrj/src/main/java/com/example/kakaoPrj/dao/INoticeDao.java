3af9877451525717a7070e52d79fb7ebfb97410f
Bae seowon <bsw0825@naver.com>
2024-05-02 12:19:18 +0900

    INoticeDao listDao 대소문자 수정

--- kakaoPrj/src/main/java/com/example/kakaoPrj/dao/INoticeDao.java
+++ kakaoPrj/src/main/java/com/example/kakaoPrj/dao/INoticeDao.java
@@ -9,7 +9,7 @@ import com.example.kakaoPrj.dto.NoticeDto;
 
 @Mapper
 public interface INoticeDao {
-	public List<NoticeDto> listDao(@Param("writer") String Writer, @Param("receiver") String receiver);
+	public List<NoticeDto> listDao(@Param("writer") String writer, @Param("receiver") String receiver);
 	public NoticeDto detailDao(@Param("nno") String nno);
 	public int writeDao(@Param("dto") NoticeDto dto );
 	public int deleteDao(@Param("nno") String nno);

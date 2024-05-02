ea893faa70279b01c34b5a37912a96e613149208
Bae seowon <bsw0825@naver.com>
2024-05-02 11:43:41 +0900

    INoticeDao listDao 수정

--- kakaoPrj/src/main/java/com/example/kakaoPrj/dao/INoticeDao.java
+++ kakaoPrj/src/main/java/com/example/kakaoPrj/dao/INoticeDao.java
@@ -3,12 +3,13 @@ package com.example.kakaoPrj.dao;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import com.example.kakaoPrj.dto.NoticeDto;
 
 @Mapper
 public interface INoticeDao {
-	public List<NoticeDto> listDao();
+	public List<NoticeDto> listDao(@Param("writer") String Writer, @Param("receiver") String receiver);
 	//public NoticeDto detailDao(String nno);
 	//public void writeDao(String title, String content, String writer, String regdate, String reciever);
 	//public void deleteDao(String nno);
